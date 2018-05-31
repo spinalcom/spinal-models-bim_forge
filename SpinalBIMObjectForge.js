004cf59aefc8798c1ad5be7027029b29c89fbb10
waiss67 <baqui.waiss@gmail.com>
2018-05-31 11:57:35 +0200

    Update SpinalBIMObjectForge.js

--- SpinalBIMObjectForge.js
+++ SpinalBIMObjectForge.js
@@ -1,6 +1,12 @@
 const spinalCore = require("spinal-core-connectorjs");
 const SpinalModelsBIMViewer = require("SpinalModelsBIMViewer");
 
+// id: _id || 0,
+// name: _name || "",
+// group: new Ptr(_group),
+// display: _display || true,
+// color: _color || ""
+
 class SpinalBIMObjectForge extends SpinalModelsBIMViewer.SpinalBIMObject {
   constructor(_id, _name, _group, _display, _color, name =
     "SpinalBIMObjectForge") {
